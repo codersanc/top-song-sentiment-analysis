cbac84c95c138a532145379b02cb74f22f0f0427
Sanchit Kumar <sanc.kumar@gmail.com>
2019-04-22 17:30:37 -0400

    Adding lyrics to json dump

--- billboard_scrape.py
+++ billboard_scrape.py
@@ -1,55 +1,48 @@
 from bs4 import BeautifulSoup
 import requests
+import lyricsgenius
 import json
 
-yearend = 2013
+yearend = 2018
 
 all_my_data = []
 
 while yearend <= 2018:
-	print(yearend)
 	url = "https://www.billboard.com/charts/year-end/" + str(yearend) + "/hot-100-songs"
-	print(url)
 	results_page = requests.get(url)
 	page_html = results_page.text
 	soup = BeautifulSoup(page_html, "html.parser")
 
 	all_labels = soup.find_all("article", attrs = {'class':'ye-chart-item'})
-
+	top_10_count = 0
 	for a_div in all_labels:
-		# print(a_div)
-		# rank = a_div['data-rank']
 		mydata = {
 			"year-end": yearend,
 			"rank" : None,
 			"artist_name" : None,
-			"song_title" : None
+			"song_title" : None,
+			"lyrics" : None
 		}
 		print(yearend)
 		mydata['rank'] = a_div.find('div', attrs = {'class':'ye-chart-item__rank'}).text.replace('\n','')
 		mydata['artist_name'] = a_div.find('div', attrs = {'class':'ye-chart-item__artist'}).text.replace('\n','')
 		mydata['song_title'] = a_div.find('div', attrs = {'class':'ye-chart-item__title'}).text.replace('\n','')
 		
+		genius = lyricsgenius.Genius("SDz9JqIhSC4EmyYkkYUXieoujaqWqffF76nK73StrCPYj5qECgRImHt875qQepq3")
+		song = genius.search_song(mydata['song_title'], mydata['artist_name'])
+		if song is not None:
+			mydata['lyrics'] = song.lyrics
+		else:
+			mydata['lyrics'] = ""
+
 		all_my_data.append(mydata)
+		top_10_count = top_10_count + 1
+
+		if top_10_count == 10:
+			break;
 
 	yearend = yearend + 1
-
-
-
-
-
-		#printing the results/---
-
-		# print('Rank:',rank.text.replace('\n',''))
-		# print('Artist:',artist_name.text.replace('\n',''))
-		# print('Song:',song_title.text.replace('\n',''))
-	
-		# print('------------')
-
 
 my_open_file = open('webscraping_data.json','w')
 
 json.dump(all_my_data,my_open_file,indent=2)
-
-	
-	
